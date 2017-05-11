71d446782a1a289613c54994534cf1363de508ec
DELAJ161 <John.X.Dela.Rosa.-ND@disney.com>
2017-05-12 04:03:40 +0800

    Added form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') userForm: NgForm;
+  subscriptions = ['', 'basic', 'advanced', 'pro'];
+  subscription = 'advanced';
+  email = '';
+  submitted = false
 
+   user ={
+    email: '',
+    subscription: '',
+    secret: '',
+   };
+
+  onSubmit() {
+    console.log(this.userForm);
+    this.submitted = true;
+    this.user.email = this.userForm.value.email;
+    this.user.subscription = this.userForm.value.subscription;
+    this.user.secret = this.userForm.value.secret;
+
+    this.userForm.reset();
+  }
 }
